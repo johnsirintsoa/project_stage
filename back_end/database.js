33f61d860a0feae248f4942d076a7299f64960b7
johnsirintsoa <johnsirintsoa18@gmail.com>
2023-10-26 19:38:33 +0300

    UPDATE

--- back_end/database.js
+++ back_end/database.js
@@ -7,10 +7,38 @@ var mysql = require('mysql');
 const db_name = process.env.DB_APP
 const rohi_db = process.env.DB_AUTORITE
 
+// var conn = mysql.createConnection({
+//   host: 'localhost', // Replace with your host name
+//   user: 'root',      // Replace with your database username
+//   password: 'root',      // Replace with your database password
+//   // database: 'stage', // // Replace with your database Name
+//   database: db_name, // // Replace with your database Name
+//   timezone: 'EAT'
+// }); 
+// conn.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Database is connected successfully in ',db_name);
+// });
+
+
+// var rohi = mysql.createConnection({
+//   host: 'localhost', // Replace with your host name
+//   user: 'root',      // Replace with your database username
+//   password: 'root',      // Replace with your database password
+//   // database: 'stage', // // Replace with your database Name
+//   database: rohi_db, // // Replace with your database Name
+//   timezone: 'EAT'
+// }); 
+// rohi.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Database is connected successfully in ',rohi_db);
+// });
+
 var conn = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: 'root',      // Replace with your database password
+  host: '5.183.8.211', // Replace with your host name
+  user: 'dev',      // Replace with your database username
+  password: 'dev',      // Replace with your database password
+  port: '3306',
   // database: 'stage', // // Replace with your database Name
   database: db_name, // // Replace with your database Name
   timezone: 'EAT'
@@ -22,9 +50,10 @@ conn.connect(function(err) {
 
 
 var rohi = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: 'root',      // Replace with your database password
+  host: '5.183.8.211', // Replace with your host name
+  port: '3306',
+  user: 'dev',      // Replace with your database username
+  password: 'dev',      // Replace with your database password
   // database: 'stage', // // Replace with your database Name
   database: rohi_db, // // Replace with your database Name
   timezone: 'EAT'
