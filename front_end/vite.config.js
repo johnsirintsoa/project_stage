2146d7e5759c93edb9ab6b63044a68f023d3ca5a
johnsirintsoa <johnsirintsoa18@gmail.com>
2023-10-28 18:26:18 +0300

    update asa

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  mode: 'production',
   base: '/ProjectStageJohns/',
   // base:'/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
